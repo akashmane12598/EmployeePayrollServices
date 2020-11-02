f9d6ce4eeb0c88a954e945ab36bd689608b03ef5
Akash <akashmane12598@gmail.com>
2020-11-02 18:20:06 +0530

    [Akash] Add. Retrieve Data from DB UC2

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -7,7 +7,7 @@ namespace EmployeePayrollService
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Payroll Service!");
-            //EmployeeRepo repo = new EmployeeRepo();
+            EmployeeRepo repo = new EmployeeRepo();
 
             repo.GetAllEmployee();
         }
